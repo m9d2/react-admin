4f42f03f997e6dc25ea0f2a8173b54642e047c86
gaoy <158612783@qq.com>
2024-04-10 23:16:01 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,49 @@
-import {App as AntdApp, ConfigProvider, ThemeConfig} from "antd";
+import { App as AntdApp, ConfigProvider, ThemeConfig } from "antd";
 import zhCN from "antd/locale/zh_CN";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "@/store";
-import {RouterProvider} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import router from "@/router";
-import { theme } from 'antd';
+import { theme } from "antd";
 
 const themeConfig: ThemeConfig = {
-    cssVar: true,
-    algorithm: [theme.defaultAlgorithm],
-    token: {
-        borderRadius: 4,
-        colorPrimary: '#0060b7',
-        fontSize: 12,
-        colorLink: '#0060b7',
-        colorText: '#333333'
+  cssVar: true,
+  algorithm: [theme.defaultAlgorithm],
+  token: {
+    borderRadius: 4,
+    colorPrimary: "#0060b7",
+    fontSize: 12,
+    colorLink: "#0060b7",
+    colorText: "#333333",
+  },
+  components: {
+    Menu: {
+      subMenuItemBorderRadius: 4,
+      itemBorderRadius: 4,
     },
-    components: {
-        Menu: {
-            subMenuItemBorderRadius: 4,
-            itemBorderRadius: 4,
-        },
-        Input: {
-        },
-        Button: {
-            paddingBlock: 0,
-            contentLineHeight: 1,
-        },
-        Pagination: {
-        },
-        Table: {
-            cellPaddingBlock: 12,
-        },
-        Card: {
-        },
-
+    Input: {},
+    Button: {
+      paddingBlock: 0,
+      contentLineHeight: 1,
     },
-}
+    Pagination: {},
+    Table: {
+      cellPaddingBlock: 12,
+    },
+    Card: {},
+  },
+};
 
 function App() {
-    return (
-        <ConfigProvider locale={zhCN} theme={themeConfig}>
-            <Provider store={store}>
-                <AntdApp style={{height: '100%'}}>
-                    <RouterProvider router={router}>
-                    </RouterProvider>
-                </AntdApp>
-            </Provider>
-        </ConfigProvider>
-    )
+  return (
+    <ConfigProvider locale={zhCN} theme={themeConfig}>
+      <Provider store={store}>
+        <AntdApp style={{ height: "100%" }}>
+          <RouterProvider router={router}></RouterProvider>
+        </AntdApp>
+      </Provider>
+    </ConfigProvider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/sys/user/components/edit-form.tsx
+++ src/pages/sys/user/components/edit-form.tsx
@@ -1,53 +1,68 @@
 import React from "react";
-import { Form, Input, Modal, Switch, } from "antd";
+import { Col, Form, Input, Modal, Row, Space, Switch } from "antd";
 
 export default function AddForm(props: {
-    open: boolean | undefined,
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>,
-    handleOk: () => void,
+  open: boolean | undefined;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  handleOk: () => void;
 }) {
-    const { open, setOpen } = props
+  const { open, setOpen } = props;
 
-    const handleOk = () => {
-        setOpen(false)
-    }
+  const handleOk = () => {
+    setOpen(false);
+  };
+
+  const layout = {
+    labelCol: { span: 6 },
+    wrapperCol: { span: 12 },
+  };
+
+  const Title = () => {
     return (
-        <Modal
-            open={open}
-            title="Title"
-            onOk={handleOk}
-            onCancel={() => setOpen(false)}
-            footer={(_, { OkBtn, CancelBtn }) => (
-                <>
-                    <CancelBtn />
-                    <OkBtn />
-                </>
-            )}
-        >
-            <Form
-                labelCol={{ span: 6 }}
-                wrapperCol={{ span: 12 }}
-            >
-
-                <Form.Item label="用户名">
-                    <Input />
-                </Form.Item>
-                <Form.Item label="姓名">
-                    <Input />
-                </Form.Item>
-                <Form.Item label="密码">
-                    <Input />
-                </Form.Item>
-                <Form.Item label="手机号">
-                    <Input />
-                </Form.Item>
-                <Form.Item label="邮箱">
-                    <Input />
-                </Form.Item>
-                <Form.Item label="启用">
-                    <Switch defaultChecked />
-                </Form.Item>
-            </Form>
-        </Modal>
+        <div style={{borderBottom: '1px solid rgba(5, 5, 5, 0.06)', marginBottom: '20px', paddingBottom: '10px'}}>
+            <span>新增用户</span>
+        </div>
     )
+  }
+  return (
+    <Modal
+      open={open}
+      title={<Title/>}
+      onOk={handleOk}
+      onCancel={() => setOpen(false)}
+      footer={(_, { OkBtn, CancelBtn }) => (
+        <>
+          <CancelBtn />
+          <OkBtn />
+        </>
+      )}
+    >
+      <Form>
+        <Row>
+          <Col span={10}>
+            <Form.Item label="用户名">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={10} offset={2}>
+            <Form.Item label="姓名">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={10}>
+            <Form.Item label="用户名">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={10} offset={2}>
+            <Form.Item label="姓名">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </Modal>
+  );
 }
