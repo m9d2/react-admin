41b7df4e9147db5615c49f40fddde5168704c0b5
gaoy <158612783@qq.com>
2024-08-23 17:34:43 +0800

    format

--- src/pages/sys/log/log.tsx
+++ src/pages/sys/log/log.tsx
@@ -1,9 +1,11 @@
 import { Card, Space } from 'antd';
+import Button from '@/components/button';
 
 const Log = () => {
     return (
         <Space direction="vertical" style={{ width: '100%' }}>
             <Card></Card>
+            <Button title="hello"></Button>
         </Space>
     );
 };
